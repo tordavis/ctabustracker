0ee536c1675fb69105dfc8a764fd334e0680f4c1
Tori Davis <tori.davis@berkeley.edu>
2023-02-08 19:07:43 -0600

    renamed package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name='ctabustracker',
+setup(name='ctabustracker2',
     version='0.1dev',
     description='A python wrapper for the Chicago Transit Authority\'s Bustracker API.',
     long_description='',
